395b097e1e95b816b4953a1f268a2169e7379d86
Maggie Zheng <maggiezheng108@gmail.com>
2015-09-19 19:31:14 -0700

    fixed text box

--- inputbox.py
+++ inputbox.py
@@ -28,15 +28,15 @@ def display_box(screen, message):
   fontobject = pygame.font.Font(None,18)
   pygame.draw.rect(screen, (0,0,0),
                    ((screen.get_width() / 2) - 100,
-                    (screen.get_height() / 2) - 10,
+                    (screen.get_height() / 2) + 200,
                     200,20), 0)
   pygame.draw.rect(screen, (255,255,255),
                    ((screen.get_width() / 2) - 102,
-                    (screen.get_height() / 2) - 12,
+                    (screen.get_height() / 2) + 198,
                     204,24), 1)
   if len(message) != 0:
     screen.blit(fontobject.render(message, 1, (255,255,255)),
-                ((screen.get_width() / 2) - 100, (screen.get_height() / 2) - 10))
+                ((screen.get_width() / 2) - 100, (screen.get_height() / 2) + 200))
   pygame.display.flip()
 
 def ask(screen, question):
